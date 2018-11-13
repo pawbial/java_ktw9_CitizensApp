c510484199ad14944345c28cdbb2e7d54a23ecaf
Pawel Bialas <p.cz.bialas@gmail.com>
2018-11-13 23:59:06 +0100

    Person class start

--- src/main/java/Citizens/Model/Person.java
+++ src/main/java/Citizens/Model/Person.java
@@ -1,4 +1,60 @@
 package Citizens.Model;
 
+import java.util.Date;
+
 public class Person {
+
+    private Integer id;
+    private String name;
+    private String lastName;
+    private String sex;
+    private Date birthDate;
+
+
+
+
+    public Person () {
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
